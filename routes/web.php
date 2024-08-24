f6ab911d5f8b1e28142bbdea4f3604b2c6944e87
Dennis Bhayu <dennisbayu99@gmail.com>
2024-08-24 11:30:21 +0700

    first commit, add dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Default route
+Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index']);
